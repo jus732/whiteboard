7caa95c707e26f9057fd34141cbcb00e3567afac
Justin Santiago <jus732@gmail.com>
2019-11-05 22:50:10 -0500

    added working form

--- src/app.js
+++ src/app.js
@@ -1,9 +1,56 @@
-const express = require('express');
-const app = express();
-
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+const express = require('express');
+const db = require('./db.js');
+const mongoose = require('mongoose');
 
+const Note = mongoose.model('Note');
+const app = express();
+const publicPath = path.resolve(__dirname, 'public');
 app.set('view engine', 'hbs');
+app.use(express.static(publicPath));
+app.use(express.urlencoded({extended: false}));
 
-app.use(express.urlenconded({extended: false}));
+//inserted this session
+
+app.get('/', (req, res) => {
+  Note.find({}, (err, notes) => {
+    res.render('home', {notes: notes});
+  })
+});
+
+app.post('/', (req,res) => {
+  const note = new Note({
+    title: req.body.title,
+    notes: req.body.notes
+  });
+
+  note.save((err) => {
+    if(err)
+    {
+      return console.log(err);
+    }
+    res.redirect('/');
+  });
+});
+
+// app.get('/delete/:id', (req,res) => {
+//   Note.find({req.params.id}, (err, note) => {
+//     note.remove()
+//   })
+//   ({_id: mongodb.ObjectID(req.params.id)}, (err, result) => {
+//     if(err)
+//     {
+//       return console.log(err);
+//     }
+//     console.log(req.body);
+//     res.redirect('/');
+//   })
+// });
+
+// app.get('/draw', (req,res) => {
+//
+// });
+
+
+console.log("Server started. CTRL+C to exit.")
+app.listen(process.env.PORT || 3000);
--- src/db.js
+++ src/db.js
@@ -1,19 +1,46 @@
 const mongoose = require('mongoose');
 
-const User = new.mongoose.Schema({
+// is the environment variable, NODE_ENV, set to PRODUCTION?
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ const fs = require('fs');
+ const path = require('path');
+ const fn = path.join(__dirname, '../config.json');
+ const data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/js9164';
+}
+
+mongoose.connect(dbconf);
+
+const User = new mongoose.Schema({
   username: String,
   hash: String,
+  notes: Array,
   boards: Array
 });
 
 mongoose.model('User', User);
 
-const Board = new.mongoose.Schema({
+const Note = new mongoose.Schema({
+  title: String,
+  notes: String
+});
+
+mongoose.model('Note', Note);
+
+const Board = new mongoose.Schema({
   name: String,
   createdAt: String,
   Users: Array
 });
 
 mongoose.model('Board', Board);
-
-mongoose.connect();
